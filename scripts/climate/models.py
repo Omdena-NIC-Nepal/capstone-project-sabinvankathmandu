85f7c3f25730b6cb5356a0d4dda1adf5896aacab
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-05-05 14:27:46 +0200

    import libraries for model scripts

---
+++ scripts/climate/models.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import pickle
