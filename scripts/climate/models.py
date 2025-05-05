42cb2fd4ebd27e9ff08e9c33a20cf6b91d710fd9
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-05-05 14:30:10 +0200

    Training the model gradient boost, rigdge , random forest , lassso

--- scripts/climate/models.py
+++ scripts/climate/models.py
@@ -23,3 +23,21 @@ def split_data(X, y, test_size=0.2):
     """
     return train_test_split(X, y, test_size=test_size, random_state=42)
 
+# Train Model
+def train_model(X_train, y_train, model_type="Ridge"):
+    """
+    Train a regression model
+    """
+    if model_type == "Gradient Boosting":
+        model = GradientBoostingRegressor(random_state=42)
+    elif model_type == "Ridge":
+        model = Ridge(alpha=1.0)
+    elif model_type == "Lasso":
+        model = Lasso(alpha=0.1)
+    elif model_type == "Random Forest":
+        model = RandomForestRegressor(random_state=42)
+    else:
+        raise ValueError("Choose: 'Gradient Boosting', 'Ridge', 'Lasso', or 'Random Forest'")
+
+    model.fit(X_train, y_train)
+    return model
