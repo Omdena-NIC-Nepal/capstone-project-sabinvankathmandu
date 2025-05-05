b236e4b133ecbf82458f8c00984c61a01ccaf8c9
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-05-05 15:42:08 +0200

    model preditction notebooks finitooo

--- scripts/climate/models.py
+++ scripts/climate/models.py
@@ -4,6 +4,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import pickle
+import numpy as np
 
 
 # Prepare features for model training
