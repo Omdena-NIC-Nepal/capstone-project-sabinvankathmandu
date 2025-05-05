37c7b0fae37fd5002e97de54736708f5f1535a08
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-05-05 14:28:45 +0200

    prepare feature for model trainin

--- scripts/climate/models.py
+++ scripts/climate/models.py
@@ -4,3 +4,14 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import pickle
+
+
+def prepare_features(df):
+    """
+    Prepares X and y for model training
+    """
+    target = 'avg_mean_temp'
+    features = df.drop(columns=[target])
+    X = features.select_dtypes(include=[np.number])
+    y = df[target]
+    return X, y
