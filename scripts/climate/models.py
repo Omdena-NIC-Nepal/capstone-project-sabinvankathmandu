470a1f764221f640b3d2d433152aa51cdbd152fd
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-05-05 14:30:48 +0200

    Model evaluation functino created

--- scripts/climate/models.py
+++ scripts/climate/models.py
@@ -5,16 +5,18 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 import pickle
 
+
 # Prepare features for model training
 def prepare_features(df):
     """
     Prepares X and y for model training
     """
-    target = 'avg_mean_temp'
+    target = "avg_mean_temp"
     features = df.drop(columns=[target])
     X = features.select_dtypes(include=[np.number])
     y = df[target]
     return X, y
+
 
 # Split Data
 def split_data(X, y, test_size=0.2):
@@ -22,6 +24,7 @@ def split_data(X, y, test_size=0.2):
     Split datas into train/test sets
     """
     return train_test_split(X, y, test_size=test_size, random_state=42)
+
 
 # Train Model
 def train_model(X_train, y_train, model_type="Ridge"):
@@ -37,7 +40,30 @@ def train_model(X_train, y_train, model_type="Ridge"):
     elif model_type == "Random Forest":
         model = RandomForestRegressor(random_state=42)
     else:
-        raise ValueError("Choose: 'Gradient Boosting', 'Ridge', 'Lasso', or 'Random Forest'")
+        raise ValueError(
+            "Choose: 'Gradient Boosting', 'Ridge', 'Lasso', or 'Random Forest'"
+        )
 
     model.fit(X_train, y_train)
     return model
+
+
+# Evaluate Model
+def evaluate_model(model, X_train, y_train, X_test, y_test):
+    """
+    Evaluate performance
+    """
+    y_pred_train = model.predict(X_train)
+    y_pred_test = model.predict(X_test)
+
+    metrics = {
+        "train_rmse": np.sqrt(mean_squared_error(y_train, y_pred_train)),
+        "test_rmse": np.sqrt(mean_squared_error(y_test, y_pred_test)),
+        "train_r2": r2_score(y_train, y_pred_train),
+        "test_r2": r2_score(y_test, y_pred_test),
+        "train_mae": mean_absolute_error(y_train, y_pred_train),
+        "test_mae": mean_absolute_error(y_test, y_pred_test),
+        "y_test": y_test,
+        "y_pred_test": y_pred_test,
+    }
+    return metrics
