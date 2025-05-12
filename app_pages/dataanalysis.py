8f9b136b6f902149f5579f35b991dd989111bb04
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-05-12 10:26:29 +0200

    some changes

--- app_pages/dataanalysis.py
+++ app_pages/dataanalysis.py
@@ -14,8 +14,8 @@ def show(df):
     )
     st.dataframe(df)
 
-    st.subheader("Check for null values", divider="blue")
-    st.write(check_null_values(df))
+    # st.subheader("Check for null values", divider="blue")
+    # st.write(check_null_values(df))
     st.subheader("Data after cleaning", divider="blue")
     st.write(handle_null_values(df))
     st.subheader("General Summary statistics", divider="blue")
